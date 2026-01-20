c096e03344617535e8945d26e29c0c7388d22bf2
honey61 <hs8126246@gmail.com>
2026-01-20 11:30:32 +0530

    Modified Changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/Vicky-Electronics-" element={<HomePage />} />
         <Route path="/contact" element={<ContactOsome />} />
         <Route path="/about" element={<AboutUsPage />} />
         <Route path="/electricians" element={<Electricians />} />
